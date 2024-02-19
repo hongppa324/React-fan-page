09b3618ac574fbd280381e991c586112f7911597
Peter Hong <t2023-m0052@Peter-Hong-MacBook-Air.local>
2024-02-19 21:31:19 +0900

    [MOD] Modify Router.jsx

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,23 +1,13 @@
 import Home from "pages/Home";
 import Detail from "pages/Detail";
-import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import fakeData from "fakeData.json";
 
 export default function Router() {
-  const [messages, setMessages] = useState(fakeData);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Home messages={messages} setMessages={setMessages} />}
-        />
-        <Route
-          path="/detail/:id"
-          element={<Detail messages={messages} setMessages={setMessages} />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
